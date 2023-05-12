959ae87693401b33be382a55f8b532d1a8717617
Ulu-pro <web.ulu7@gmail.com>
2023-05-12 21:11:50 +0500

    Splitting the gateways by routers

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
-from routers import read
+from routers import create, read, update, delete
 
 app = FastAPI()
 
+app.include_router(create.router)
 app.include_router(read.router)
+app.include_router(update.router)
+app.include_router(delete.router)
